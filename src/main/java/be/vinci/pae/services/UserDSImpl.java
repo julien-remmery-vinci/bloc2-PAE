2ae83e623d409195eb0c7f5fb48a91d65c80260f
0xhajar <hajar.haouat@student.vinci.be>
2024-02-21 18:57:18 +0100

    feat:add get user from db

--- src/main/java/be/vinci/pae/services/UserDSImpl.java
+++ src/main/java/be/vinci/pae/services/UserDSImpl.java
@@ -1,15 +1,61 @@
 package be.vinci.pae.services;
 
+import be.vinci.pae.business.Factory;
+import be.vinci.pae.business.User;
 import be.vinci.pae.business.UserDTO;
+import be.vinci.pae.utils.Config;
+import jakarta.inject.Inject;
+
+import java.sql.*;
 
 /**
  * Implementation of UserDS.
  */
 public class UserDSImpl implements UserDS {
-
-  @Override
-  public UserDTO getOneByEmail(String email) {
-    return null;
-  }
+    @Inject
+    private Factory factory;
+    private static PreparedStatement getUser;
+    private static Connection conn = null;
+    static {
+        String url = Config.getProperty("dbServer") + Config.getProperty("dbUser");
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            getUser = conn.prepareStatement("SELECT * from pae.users WHERE email = ?");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public UserDTO getOneByEmail(String email) {
+        UserDTO user = factory.getUser();
+        try {
+            getUser.setString(1,email);
+            try (ResultSet rs = getUser.executeQuery()) {
+                while (rs.next()) {
+                    user.setIdUser(rs.getInt(1));
+                    user.setLastname(rs.getString(2));
+                    user.setFirstname(rs.getString(3));
+                    user.setEmail(rs.getString(4));
+                    user.setPassword(rs.getString(5));
+                    user.setPhoneNumber(rs.getString(6));
+                    user.setRegisterDate(rs.getDate(7));
+                    user.setRole(UserDTO.Role.valueOf(rs.getString(8)));
+                    return user;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 
 }
