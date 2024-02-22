d7fa9269bf7918a3d9968d48fa9f53f3750152c0
Julien Remmery <julien.remmery@student.vinci.be>
2024-02-22 12:36:58 +0100

    refactor: use only presentation package classes as ressources

--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -27,7 +27,7 @@ public class Main {
   public static HttpServer startServer() {
     // create a resource config that scans for JAX-RS resources and providers
     // in be.vinci package
-    final ResourceConfig rc = new ResourceConfig().packages("be.vinci.pae")
+    final ResourceConfig rc = new ResourceConfig().packages("be.vinci.pae.presentation")
         .register(ApplicationBinder.class);
 
     // Base URI the Grizzly HTTP server will listen on
