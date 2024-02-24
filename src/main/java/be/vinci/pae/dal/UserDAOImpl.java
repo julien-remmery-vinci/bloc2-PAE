46ec9660169cd041651d7e08b2f76336e9be8ed1
Didia José Fechado <didia.josefechado@student.vinci.be>
2024-02-24 18:48:29 +0100

    fix: duplicated code UserDAO

--- src/main/java/be/vinci/pae/dal/UserDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/UserDAOImpl.java
@@ -19,19 +19,12 @@ public class UserDAOImpl implements UserDAO {
 
   @Override
   public UserDTO getOneByEmail(String email) {
-    try (PreparedStatement getUser = dalServices.getPS("SELECT * from pae.users WHERE email = ?")){
+    try (PreparedStatement getUser = dalServices.getPS("SELECT * from pae.users WHERE email = ?")) {
       getUser.setString(1, email);
       try (ResultSet rs = getUser.executeQuery()) {
         if (rs.next()) {
           UserDTO user = factory.getUser();
-          user.setIdUser(rs.getInt(1));
-          user.setLastname(rs.getString(2));
-          user.setFirstname(rs.getString(3));
-          user.setEmail(rs.getString(4));
-          user.setPassword(rs.getString(5));
-          user.setPhoneNumber(rs.getString(6));
-          user.setRegisterDate(rs.getDate(7));
-          user.setRole(UserDTO.Role.valueOf(rs.getString(8)));
+          getUserFromRs(rs, user);
           return user;
         }
       }
@@ -49,23 +42,14 @@ public class UserDAOImpl implements UserDAO {
    */
   @Override
   public UserDTO getOneById(int id) {
-    UserDTO user = factory.getUser();
     try {
-      PreparedStatement getUser = dalServices.getPrepareStatement(
+      PreparedStatement getUser = dalServices.getPS(
           "SELECT * from pae.users WHERE id_user = ?");
       getUser.setInt(1, id);
       try (ResultSet rs = getUser.executeQuery()) {
         if (rs.next()) {
-          user.setIdUser(rs.getInt(1));
-          user.setLastname(rs.getString(2));
-          user.setFirstname(rs.getString(3));
-          user.setEmail(rs.getString(4));
-          user.setPassword(rs.getString(5));
-          user.setPhoneNumber(rs.getString(6));
-          user.setRegisterDate(rs.getDate(7));
-          user.setRole(UserDTO.Role.valueOf(rs.getString(8)));
-          rs.close();
-          getUser.close();
+          UserDTO user = factory.getUser();
+          getUserFromRs(rs, user);
           return user;
         }
       }
@@ -75,4 +59,14 @@ public class UserDAOImpl implements UserDAO {
     return null;
   }
 
+  public void getUserFromRs(ResultSet rs, UserDTO user) throws SQLException {
+    user.setIdUser(rs.getInt(1));
+    user.setLastname(rs.getString(2));
+    user.setFirstname(rs.getString(3));
+    user.setEmail(rs.getString(4));
+    user.setPassword(rs.getString(5));
+    user.setPhoneNumber(rs.getString(6));
+    user.setRegisterDate(rs.getDate(7));
+    user.setRole(UserDTO.Role.valueOf(rs.getString(8)));
+  }
 }
