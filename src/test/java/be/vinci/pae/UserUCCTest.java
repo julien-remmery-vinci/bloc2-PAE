4f6767a2156c0df26fa51cdb3651e133c5d0c841
Julien Remmery <julien.remmery@student.vinci.be>
2024-02-24 17:22:35 +0100

    feat: Test for login method of UserUCC now fully working

--- src/test/java/be/vinci/pae/UserUCCTest.java
+++ src/test/java/be/vinci/pae/UserUCCTest.java
@@ -3,43 +3,39 @@ package be.vinci.pae;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import be.vinci.pae.business.Factory;
+import be.vinci.pae.business.UserDTO;
 import be.vinci.pae.business.UserUCC;
-import be.vinci.pae.services.UserDS;
-import be.vinci.pae.services.UserDSImpl;
-import be.vinci.pae.utils.ApplicationBinder;
-import be.vinci.pae.utils.Config;
+import be.vinci.pae.dal.UserDAO;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 import org.mockito.Mockito;
 
 public class UserUCCTest {
 
-  static {
-    Config.load("dev.properties");
-  }
-
   private UserUCC userUCC;
-  private UserDS userDS;
+  private UserDAO userDAO;
   private Factory factory;
 
   @BeforeEach
   void setUp() {
-    ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinder());
-
+    ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinderTest());
     this.userUCC = locator.getService(UserUCC.class);
     this.factory = locator.getService(Factory.class);
-
-    this.userDS = Mockito.mock(UserDSImpl.class);
-
-    Mockito.when(userDS.getOneByEmail("admin")).thenReturn(factory.getUser());
+    this.userDAO = locator.getService(UserDAO.class);
   }
 
   @Test
-  @DisplayName("Tests for the login method of UserUCC")
+  @DisplayName("Test for the login method of UserUCC")
   void loginTest() {
-    assertNotNull(userUCC.login("admin", "admin"));
+    UserDTO user = factory.getUser();
+    String password = "admin";
+    String email = "admin@vinci.be";
+    user.setPassword(BCrypt.hashpw(password, BCrypt.gensalt()));
+    Mockito.when(userDAO.getOneByEmail(email)).thenReturn(user);
+    assertNotNull(userUCC.login(email, password));
   }
 }
