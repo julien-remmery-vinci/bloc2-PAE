50db91261def8ea7cc06ecd68b057a92a7ae852f
Julien Remmery <julien.remmery@student.vinci.be>
2024-02-25 20:19:49 +0100

    refactor: changed columns names to match fields name in corresponding java class

--- init.sql
+++ init.sql
@@ -3,15 +3,15 @@ CREATE SCHEMA pae;
 
 CREATE TABLE pae.users
 (
-    id_user       SERIAL PRIMARY KEY,
+    idUser       SERIAL PRIMARY KEY,
     lastname      TEXT   NOT NULL,
     firstname     TEXT   NOT NULL,
     email         TEXT   NOT NULL,
     password      TEXT   NOT NULL,
-    phone_number  TEXT   NOT NULL,
-    register_date date   NOT NULL,
+    phoneNumber  TEXT   NOT NULL,
+    registerDate date   NOT NULL,
     role          TEXT   NOT NULL
 );
 
-INSERT INTO pae.users (lastname, firstname, email, password, phone_number, register_date, role)
+INSERT INTO pae.users (lastname, firstname, email, password, phoneNumber, registerDate, role)
 VALUES ('admin', 'admin', 'admin@vinci.be', '$2a$10$aqGDWyP8K9xeO1a8uPzcWuzf1hfiPU8IlAR8GyhgVIYhnuKNSxP7S', '0123456789', '2024-02-22', 'A');
