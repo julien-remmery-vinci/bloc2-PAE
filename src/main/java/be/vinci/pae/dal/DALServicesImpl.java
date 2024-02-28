d49d37916f65f91c6163ee1cadb911db5ddab49f
Julien Remmery <julien.remmery@student.vinci.be>
2024-02-28 18:04:12 +0100

    refactor: add missing javadoc comments

--- src/main/java/be/vinci/pae/dal/DALServicesImpl.java
+++ src/main/java/be/vinci/pae/dal/DALServicesImpl.java
@@ -1,7 +1,6 @@
 package be.vinci.pae.dal;
 
 import be.vinci.pae.utils.Config;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,6 +10,9 @@ public class DALServicesImpl implements DALServices {
 
   private static Connection conn = null;
 
+  /**
+   * Constructor of DALServicesImpl.
+   */
   public DALServicesImpl() {
     String url = Config.getProperty("DB_URL");
     String username = Config.getProperty("DB_USER");
@@ -23,6 +25,11 @@ public class DALServicesImpl implements DALServices {
     }
   }
 
+  /**
+   * Get the prepared statement with the request.
+   *
+   * @return the prepared statement with the request.
+   */
   public PreparedStatement getPS(String request) {
     try {
       return conn.prepareStatement(request);
