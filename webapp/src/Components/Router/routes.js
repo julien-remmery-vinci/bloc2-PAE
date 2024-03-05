d052ba31394479ae9ae065d50b28502d096aa45b
Hajar Haouat <hajar.haouat@student.vinci.be>
2024-03-05 14:10:28 +0100

    feat: add register form

--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -1,10 +1,12 @@
 import HomePage from "../Pages/HomePage";
 import LoginPage from "../Pages/LoginPage";
+import RegisterPage from "../Pages/RegisterPage";
 
 // Define routes
 const routes = {
     '/':HomePage,
-    '/login':LoginPage
+    '/login':LoginPage,
+    '/register':RegisterPage
 };
 
 export default routes;
