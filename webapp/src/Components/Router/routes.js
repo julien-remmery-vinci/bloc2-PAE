4db5f6d4d5e2f264eefb2c95532f8688c8f69673
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-05 15:16:48 +0100

    feat: add route for contact refusal page

--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -1,10 +1,12 @@
 import HomePage from "../Pages/HomePage";
 import LoginPage from "../Pages/LoginPage";
+import AcceptRefusePage from "../Pages/contacts/AcceptRefusePage";
 
 // Define routes
 const routes = {
     '/':HomePage,
-    '/login':LoginPage
+    '/login':LoginPage,
+    '/contact/refusal': AcceptRefusePage
 };
 
 export default routes;
