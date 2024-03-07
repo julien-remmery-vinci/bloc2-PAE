6984070ae9cec8523657796220d8f45313445168
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-07 15:47:51 +0100

    feat: add refuse contact route

--- src/main/java/be/vinci/pae/presentation/ContactRessource.java
+++ src/main/java/be/vinci/pae/presentation/ContactRessource.java
@@ -1,9 +1,41 @@
 package be.vinci.pae.presentation;
 
 
+import be.vinci.pae.business.contact.ContactDTO;
+import be.vinci.pae.business.contact.ContactUCC;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
+
 /**
  * addContact route.
  */
+@Singleton
+@Path("/contact")
 public class ContactRessource {
+
+  @Inject
+  private ContactUCC contactUCC;
+
+  /**
+   * Refuse a contact.
+   *
+   * @param id the id of the contact
+   * @return the contact
+   */
+  @Path("/{id}/accept")
+  public ContactDTO refuseContact(@PathParam("id") int id) {
+    if (id < 0) {
+      throw new WebApplicationException("Invalid id", Status.BAD_REQUEST);
+    }
+    ContactDTO contact = contactUCC.refuseContact(id);
+    if (contact == null) {
+      throw new WebApplicationException("Contact not found", Status.NOT_FOUND);
+    }
+    return contact;
+  }
 
 }
