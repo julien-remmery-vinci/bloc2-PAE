91ad3b18a2e29d385425e5fa52052f6f8ec5109e
Didia José Fechado <didia.josefechado@student.vinci.be>
2024-03-07 16:10:51 +0100

    feat: add profile page without fetch

--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -2,13 +2,15 @@ import HomePage from "../Pages/HomePage";
 import LoginPage from "../Pages/LoginPage";
 import RegisterPage from "../Pages/RegisterPage";
 import AcceptRefusePage from "../Pages/contacts/AcceptRefusePage";
+import ProfilePage from "../Pages/ProfilePage";
 
 // Define routes
 const routes = {
     '/':HomePage,
     '/login':LoginPage,
     '/contact/refusal': AcceptRefusePage,
-    '/register':RegisterPage
+    '/register':RegisterPage,
+    '/profile': ProfilePage,
 };
 
 export default routes;
