ef8cecd0854433c15c60aa267d38581ff9c665a8
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-07 22:39:26 +0100

    fix: add annotations to refuseContact ressource

--- src/main/java/be/vinci/pae/presentation/ContactRessource.java
+++ src/main/java/be/vinci/pae/presentation/ContactRessource.java
@@ -3,11 +3,16 @@ package be.vinci.pae.presentation;
 
 import be.vinci.pae.business.contact.ContactDTO;
 import be.vinci.pae.business.contact.ContactUCC;
+import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response.Status;
 
 /**
@@ -26,12 +31,16 @@ public class ContactRessource {
    * @param id the id of the contact
    * @return the contact
    */
+  @POST
   @Path("/{id}/accept")
-  public ContactDTO refuseContact(@PathParam("id") int id, String refusalReason) {
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public ContactDTO refuseContact(@PathParam("id") int id, JsonNode json) {
     if (id < 0) {
       throw new WebApplicationException("Invalid id", Status.BAD_REQUEST);
     }
-    if (refusalReason == null || refusalReason.isEmpty()) {
+    String refusalReason = json.get("refusalReason").asText();
+    if (!json.hasNonNull("refusalReason") || refusalReason.isBlank()) {
       throw new WebApplicationException("Refusal reason is required", Status.BAD_REQUEST);
     }
     ContactDTO contact = contactUCC.refuseContact(id, refusalReason);
