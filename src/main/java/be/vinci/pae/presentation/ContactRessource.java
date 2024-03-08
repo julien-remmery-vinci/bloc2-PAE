8d5de24e84f288d46486892d64c9ab6e602a9493
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-08 21:38:17 +0100

    feat: get request context to get user id

--- src/main/java/be/vinci/pae/presentation/ContactRessource.java
+++ src/main/java/be/vinci/pae/presentation/ContactRessource.java
@@ -3,6 +3,8 @@ package be.vinci.pae.presentation;
 
 import be.vinci.pae.business.contact.ContactDTO;
 import be.vinci.pae.business.contact.ContactUCC;
+import be.vinci.pae.business.user.UserDTO;
+import be.vinci.pae.presentation.filters.Authorize;
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -12,8 +14,10 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response.Status;
+import org.glassfish.jersey.server.ContainerRequest;
 
 /**
  * addContact route.
@@ -28,22 +32,27 @@ public class ContactRessource {
   /**
    * Refuse a contact.
    *
-   * @param id the id of the contact
+   * @param request   the request's context
+   * @param idContact the id of the contact
+   * @param json      json containing the refusal reason
    * @return the contact
    */
   @POST
   @Path("/{id}/refuse")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
-  public ContactDTO refuseContact(@PathParam("id") int id, JsonNode json) {
-    if (id < 0) {
+  @Authorize
+  public ContactDTO refuseContact(@Context ContainerRequest request, @PathParam("id") int idContact,
+      JsonNode json) {
+    if (idContact < 0) {
       throw new WebApplicationException("Invalid id", Status.BAD_REQUEST);
     }
     String refusalReason = json.get("refusalReason").asText();
     if (!json.hasNonNull("refusalReason") || refusalReason.isBlank()) {
       throw new WebApplicationException("Refusal reason is required", Status.BAD_REQUEST);
     }
-    ContactDTO contact = contactUCC.refuseContact(id, refusalReason);
+    ContactDTO contact = contactUCC.refuseContact(idContact, refusalReason,
+        ((UserDTO) request.getProperty("user")).getIdUser());
     if (contact == null) {
       throw new WebApplicationException("Contact not found", Status.NOT_FOUND);
     }
