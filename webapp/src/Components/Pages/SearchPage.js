32fba50196f51f565847d9b8cdaae251f3ae90a6
Semih Türkoglu <semih.turkoglu@student.vinci.be>
2024-03-10 18:55:13 +0100

    feat: adding blank filters to SearchPage

--- webapp/src/Components/Pages/SearchPage.js
+++ webapp/src/Components/Pages/SearchPage.js
@@ -1,10 +1,28 @@
+import { clearPage, renderPageTitle } from "../../utils/render";
+
 const SearchPage = () => {
+  renderPageTitle("Recherche");
+  clearPage();
   renderSearchPage();
 };
 
 function renderSearchPage() {
   const main = document.querySelector('main');
   main.innerHTML = `
+  <div class="search-container d-flex justify-content-between">
+  <div class="filter-container">
+    <h3>Filtres</h3>
+    <label>
+      <input type="checkbox" name="filter" value="etudiant">
+      Que les étudiants
+    </label>
+  </div>
+
+  <div class="search-bar">
+    <input class="form-control border-end-0 border rounded-pill" type="search" value="Rechercher">
+  </div>
+</div>
+<br>
     <div class="table-responsive d-flex justify-content-center align-items-center">
     <table class="table table-bordered table-hover">
     <thead>
