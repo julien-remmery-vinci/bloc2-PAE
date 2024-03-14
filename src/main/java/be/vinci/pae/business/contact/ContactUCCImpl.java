6031c3fd085289db32c4f55e881bbe69b33dba20
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-14 15:15:16 +0100

    refactor: use better status code

--- src/main/java/be/vinci/pae/business/contact/ContactUCCImpl.java
+++ src/main/java/be/vinci/pae/business/contact/ContactUCCImpl.java
@@ -29,7 +29,7 @@ public class ContactUCCImpl implements ContactUCC {
 
     if (!contact.getState().equals(Contact.STATE_TAKEN)) {
       throw new WebApplicationException("The contact must be in the state 'taken' to be refused",
-          Status.BAD_REQUEST);
+          Status.PRECONDITION_FAILED);
     }
     contact.setState(Contact.STATE_TAKENDOWN);
     contact.setRefusalReason(refusalReason);
