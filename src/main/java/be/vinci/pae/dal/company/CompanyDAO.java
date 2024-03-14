d2c599387189bb66128ff1d2784bc722265ad656
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-03-14 12:12:07 +0100

    feat: Add getAll() method to CompanyDAO interface and implement it in CompanyDAOImpl

--- src/main/java/be/vinci/pae/dal/company/CompanyDAO.java
+++ src/main/java/be/vinci/pae/dal/company/CompanyDAO.java
@@ -1,8 +1,18 @@
 package be.vinci.pae.dal.company;
 
+import be.vinci.pae.business.company.CompanyDTO;
+import java.util.List;
+
 /**
  * Interface of CompanyDAOImpl.
  */
 public interface CompanyDAO {
+
+  /**
+   * Get all companies.
+   *
+   * @return all companies
+   */
+  List<CompanyDTO> getAll();
 
 }
