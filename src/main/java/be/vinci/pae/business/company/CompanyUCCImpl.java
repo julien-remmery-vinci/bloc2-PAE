2f2e1fe261760ddf88231e1d7f79fc5903c3e0ff
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-03-14 12:09:45 +0100

    feat: Add getAll() method to CompanyUCC interface and implement it in CompanyUCCImpl

--- src/main/java/be/vinci/pae/business/company/CompanyUCCImpl.java
+++ src/main/java/be/vinci/pae/business/company/CompanyUCCImpl.java
@@ -1,8 +1,19 @@
 package be.vinci.pae.business.company;
 
+import be.vinci.pae.dal.company.CompanyDAO;
+import jakarta.inject.Inject;
+import java.util.List;
+
 /**
  * Implementation of CompanyUCC.
  */
 public class CompanyUCCImpl implements CompanyUCC {
+
+  @Inject
+  private CompanyDAO companyDAO;
+
+  public List<CompanyDTO> getAll() {
+    return companyDAO.getAll();
+  }
 
 }
