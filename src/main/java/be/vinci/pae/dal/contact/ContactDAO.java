00b0efa7feae9d5806285357c630460cbb269112
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-16 15:33:25 +0100

    feat: add getContactFromRs in ContactDAO

--- src/main/java/be/vinci/pae/dal/contact/ContactDAO.java
+++ src/main/java/be/vinci/pae/dal/contact/ContactDAO.java
@@ -1,11 +1,19 @@
 package be.vinci.pae.dal.contact;
 
 import be.vinci.pae.business.contact.ContactDTO;
+import java.sql.ResultSet;
 
 /**
  * Interface of ContactDAOImpl.
  */
 public interface ContactDAO {
+
+  /**
+   * Get a contact from a ResultSet.
+   *
+   * @return the contact
+   */
+  ContactDTO getContactFromRs(ResultSet rs);
 
   /**
    * Get a contact by its id.
