f2ab5bf2afb4fe0ad954561caf9ba577169311eb
Julien Remmery <julien.remmery@student.vinci.be>
2024-03-16 15:33:06 +0100

    feat: add getCompanyFromRs in CompanyDAO

--- src/main/java/be/vinci/pae/dal/company/CompanyDAO.java
+++ src/main/java/be/vinci/pae/dal/company/CompanyDAO.java
@@ -1,12 +1,20 @@
 package be.vinci.pae.dal.company;
 
 import be.vinci.pae.business.company.CompanyDTO;
+import java.sql.ResultSet;
 import java.util.List;
 
 /**
  * Interface of CompanyDAOImpl.
  */
 public interface CompanyDAO {
+
+  /**
+   * Get a company from a ResultSet.
+   *
+   * @return the company
+   */
+  CompanyDTO getCompanyFromRs(ResultSet rs);
 
   /**
    * Get all companies.
