e8848f02e2273d44d4c161f414847cd2ccc8cb87
Hajar <hajar.haouat@student.vinci.be>
2024-03-16 16:24:15 +0100

    fix: add DALServices bind in tests

--- src/main/java/be/vinci/pae/dal/DALServices.java
+++ src/main/java/be/vinci/pae/dal/DALServices.java
@@ -1,9 +1,22 @@
 package be.vinci.pae.dal;
 
 public interface DALServices {
+    /**
+     * Get the connection to the database.
+     *
+     * @return the connection to the database
+     */
     void start();
-
+    /**
+     * Get the connection to the database.
+     *
+     * @return the connection to the database
+     */
     void commit();
-
+    /**
+     * Get the connection to the database.
+     *
+     * @return the connection to the database
+     */
     void rollback();
 }
