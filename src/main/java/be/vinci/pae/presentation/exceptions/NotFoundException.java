099e87c24ba51e8861e951b829b7907d81f83f82
Hajar Haouat <hajar.haouat@student.vinci.be>
2024-03-23 21:21:15 +0100

    fix: fix style errors

--- src/main/java/be/vinci/pae/presentation/exceptions/NotFoundException.java
+++ src/main/java/be/vinci/pae/presentation/exceptions/NotFoundException.java
@@ -6,35 +6,35 @@ import jakarta.ws.rs.core.Response.Status;
 
 public class NotFoundException extends WebApplicationException {
 
-    /**
-     * Constructor without a message.
-     */
-    public NotFoundException() {
-        super(Response.status(Status.NOT_FOUND)
-                .build());
-    }
+  /**
+   * Constructor without a message.
+   */
+  public NotFoundException() {
+    super(Response.status(Status.NOT_FOUND)
+        .build());
+  }
 
-    /**
-     * Constructor with a message.
-     *
-     * @param message the message
-     */
-    public NotFoundException(String message) {
-        super(Response.status(Status.NOT_FOUND)
-                .entity(message)
-                .type("text/plain")
-                .build());
-    }
+  /**
+   * Constructor with a message.
+   *
+   * @param message the message
+   */
+  public NotFoundException(String message) {
+    super(Response.status(Status.NOT_FOUND)
+        .entity(message)
+        .type("text/plain")
+        .build());
+  }
 
-    /**
-     * Constructor with a cause.
-     *
-     * @param cause the cause
-     */
-    public NotFoundException(Throwable cause) {
-        super(Response.status(Status.NOT_FOUND)
-                .entity(cause.getMessage())
-                .type("text/plain")
-                .build());
-    }
+  /**
+   * Constructor with a cause.
+   *
+   * @param cause the cause
+   */
+  public NotFoundException(Throwable cause) {
+    super(Response.status(Status.NOT_FOUND)
+        .entity(cause.getMessage())
+        .type("text/plain")
+        .build());
+  }
 }
