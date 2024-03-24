c3cce7b618bfaa1406f54e68e0c3f4c4457a3362
Hajar <hajar.haouat@student.vinci.be>
2024-03-24 03:34:38 +0100

    feat: add navbar for teacher

--- webapp/src/Components/Navbar/Navbar.js
+++ webapp/src/Components/Navbar/Navbar.js
@@ -21,7 +21,7 @@ async function renderNavbar() {
     </nav>
   `;
 
-  const loggedNavbar = `
+  const loggedNavbarStudent = `
   <nav class="navbar navbar-expand-lg navbar-light bg-primary-subtle">
     <a class="navbar-brand" href="#">VinciOBS</a>
     <div class="ms-auto">
@@ -35,6 +35,28 @@ async function renderNavbar() {
     </div>
   </nav>
   `;
+
+  const loggedNavbarTeacher = `
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary-subtle">
+    <a class="navbar-brand" href="#">VinciOBS</a>
+    <div class="ms-auto">
+      <button class="btn btn-primary rounded" type="button" id="button1">
+        Profil
+      </button>
+      <button class="btn btn-primary rounded" type="button" id="recherches">
+        Recherches
+      </button>
+       <button class="btn btn-primary rounded" type="button" id="responsables">
+        Responsables
+       </button>
+       <button class="btn btn-primary rounded" type="button" id="stats">
+        Statistiques
+       </button>
+        <button class="btn btn-primary rounded" type="button" id="button2">Déconnexion</button>
+    </div>
+  </nav>
+  `;
+
   const navbar = document.querySelector('#navbarWrapper');
   if (getUserToken() === undefined) {
     navbar.innerHTML = notLoggedNavbar;
@@ -46,8 +68,8 @@ async function renderNavbar() {
     registerButton.addEventListener('click', () => {
       Navigate('/register');
     });
-  } else {
-    navbar.innerHTML = loggedNavbar;
+  } else if (getUserToken().role === 'student') {
+    navbar.innerHTML = loggedNavbarStudent;
     const logOutButton = document.querySelector('#button2');
     const profileButton = document.querySelector('#button1');
     const contactsButton = document.querySelector('#contacts');
@@ -61,6 +83,30 @@ async function renderNavbar() {
     });
     contactsButton.addEventListener('click', () => {
       Navigate('/contact');
+    });
+  } else if (getUserToken().role === 'teacher') {
+    navbar.innerHTML = loggedNavbarTeacher;
+    const logOutButton = document.querySelector('#button2');
+    const profileButton = document.querySelector('#button1');
+    const researchesButton = document.querySelector('#recherches');
+    const responsablesButton = document.querySelector('#responsables');
+    const statsButton = document.querySelector('#stats');
+    logOutButton.addEventListener('click', () => {
+      clearAuthenticatedUser();
+      Navigate('/login');
+      window.location.reload();
+    });
+    profileButton.addEventListener('click', () => {
+      Navigate('/profile');
+    });
+    researchesButton.addEventListener('click', () => {
+      Navigate('/research');
+    });
+    responsablesButton.addEventListener('click', () => {
+      Navigate('/');
+    });
+    statsButton.addEventListener('click', () => {
+      Navigate('/');
     });
   }
 }
