6305d1d07d9952fecdd3b622bc935ff2766dffda
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-03-26 00:58:09 +0100

    feat: Add ContactUCCTest methodsgor addContact

--- src/test/java/be/vinci/pae/ContactUCCTest.java
+++ src/test/java/be/vinci/pae/ContactUCCTest.java
@@ -6,10 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import be.vinci.pae.business.Factory;
+import be.vinci.pae.business.company.Company;
+import be.vinci.pae.business.company.CompanyUCC;
 import be.vinci.pae.business.contact.Contact;
 import be.vinci.pae.business.contact.ContactDTO.State;
 import be.vinci.pae.business.contact.ContactUCC;
+import be.vinci.pae.dal.company.CompanyDAO;
 import be.vinci.pae.dal.contact.ContactDAO;
+import be.vinci.pae.presentation.exceptions.PreconditionFailedException;
 import jakarta.ws.rs.WebApplicationException;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
@@ -27,14 +31,20 @@ public class ContactUCCTest {
   private final int idUser = 1;
   private final String refusalReason = "refusalReason";
   Contact contact;
+  Contact contact2;
+  Company company;
   private ContactUCC contactUCC;
   private ContactDAO contactDAO;
+  private CompanyUCC companyUCC;
+  private CompanyDAO companyDAO;
 
   @BeforeEach
   void setUp() {
     ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinderTest());
     contactUCC = locator.getService(ContactUCC.class);
     contactDAO = locator.getService(ContactDAO.class);
+    companyUCC = locator.getService(CompanyUCC.class);
+    companyDAO = locator.getService(CompanyDAO.class);
     Factory factory = locator.getService(Factory.class);
 
     contact = (Contact) factory.getContact();
@@ -42,6 +52,53 @@ public class ContactUCCTest {
     contact.setIdStudent(idUser);
     contact.setState(State.ADMITTED);
     Mockito.when(contactDAO.getOneById(1)).thenReturn(contact);
+
+    contact2 = (Contact) factory.getContact();
+    contact2.setIdContact(2);
+    contact2.setIdStudent(idUser);
+    contact2.setIdCompany(1);
+    contact2.setAcademicYear("2021-2022");
+    Mockito.when(contactDAO.getContactAccepted(idUser)).thenReturn(contact2);
+
+    company = (Company) factory.getCompany();
+    company.setDesignation("company");
+    company.setIdCompany(1);
+
+    Mockito.when(companyDAO.getCompanyById(1)).thenReturn(company);
+  }
+
+  @Test
+  @DisplayName("Test addContact with all conditions met")
+  void testAddContact() {
+    contact.setIdCompany(1);
+    contact.setAcademicYear("2021-2022");
+    Mockito.when(contactDAO.addContact(contact)).thenReturn(contact);
+    Mockito.when(contactDAO.getContactAccepted(idUser)).thenReturn(null);
+    Mockito.when(contactDAO.getCompanyContact(idUser, 1, "2021-2022")).thenReturn(null);
+    Mockito.when(contactDAO.getOneById(1)).thenReturn(contact);
+    assertAll(
+        () -> assertEquals(contact.getIdContact(), contactUCC.addContact(contact).getIdContact()),
+        () -> assertEquals(State.STARTED, contact.getState())
+    );
+  }
+
+  @Test
+  @DisplayName("Test addContact with company already in contact")
+  void testAddContactCompanyAlreadyInContact() {
+    contact.setIdCompany(1);
+    contact.setAcademicYear("2021-2022");
+    Mockito.when(contactDAO.getContactAccepted(idUser)).thenReturn(null);
+    Mockito.when(contactDAO.getCompanyContact(idUser, 1, contact2.getAcademicYear()))
+        .thenReturn(contact2);
+    assertThrows(PreconditionFailedException.class, () -> contactUCC.addContact(contact));
+  }
+
+  @Test
+  @DisplayName("Test addContact when there is already a contact accepted")
+  void testAddContactAlreadyAccepted() {
+    contact.setIdCompany(1);
+    Mockito.when(contactDAO.getContactAccepted(idUser)).thenReturn(contact);
+    assertThrows(PreconditionFailedException.class, () -> contactUCC.addContact(contact));
   }
 
   @Test
@@ -68,6 +125,7 @@ public class ContactUCCTest {
         () -> assertEquals(refusalReason, contact.getRefusalReason())
     );
   }
+
 
   @Test
   @DisplayName("Test addContact with company not found")
