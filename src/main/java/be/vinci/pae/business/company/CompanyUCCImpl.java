b3c9272a3363c4f0a38a764c19028f3542d81952
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-03-25 23:46:04 +0100

    fix: checkstyle

--- src/main/java/be/vinci/pae/business/company/CompanyUCCImpl.java
+++ src/main/java/be/vinci/pae/business/company/CompanyUCCImpl.java
@@ -9,27 +9,20 @@ import java.util.List;
  * Implementation of CompanyUCC.
  */
 public class CompanyUCCImpl implements CompanyUCC {
+
   @Inject
   private DALServices dalServices;
 
   @Inject
   private CompanyDAO companyDAO;
-  /**
-   * Get all companies.
-   *
-   * @return all companies
-   */
+
   public List<CompanyDTO> getAll() {
     List<CompanyDTO> companies = companyDAO.getAll();
     dalServices.close();
     return companies;
   }
-  /**
-   * Get a company by its id.
-   *
-   * @param id the id of the company
-   * @return the company, null if no company was found
-   */
+
+
   public CompanyDTO getCompanyById(int id) {
     CompanyDTO company = companyDAO.getCompanyById(id);
     dalServices.close();
