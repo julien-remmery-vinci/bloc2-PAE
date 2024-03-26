515a9997a1a35d4e42716e1da0b2e9dbdcd8916e
Didia José Fechado <didia.josefechado@student.vinci.be>
2024-03-27 00:07:32 +0100

    refactor: internshipSupervisor

--- src/main/java/be/vinci/pae/business/internshipsupervisor/InternshipSupervisorUCCImpl.java
+++ src/main/java/be/vinci/pae/business/internshipsupervisor/InternshipSupervisorUCCImpl.java
@@ -3,4 +3,6 @@ package be.vinci.pae.business.internshipsupervisor;
 /**
  * Implementation of InternshipSupervisorUCC.
  */
+public class InternshipSupervisorUCCImpl implements InternshipSupervisorUCC {
 
+}
--- src/main/java/be/vinci/pae/dal/internshipsupervisor/InternshipSupervisorDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/internshipsupervisor/InternshipSupervisorDAOImpl.java
@@ -1,18 +1,9 @@
 package be.vinci.pae.dal.internshipsupervisor;
 
-import be.vinci.pae.dal.DALBackServices;
-import be.vinci.pae.dal.utils.DAOServices;
-import jakarta.inject.Inject;
-
 /**
  * Implementation of InternshipSupervisorDAOImpl.
  */
 public class InternshipSupervisorDAOImpl {
-
-  @Inject
-  private DALBackServices dalBackServices;
-  @Inject
-  private DAOServices daoServices;
 
 
 }
