80a48b2c714641fb51522c5a3f4a4f6297fbbf71
Hajar <hajar.haouat@student.vinci.be>
2024-03-26 20:40:12 +0100

    fix: style errors

--- src/main/java/be/vinci/pae/presentation/UserRessource.java
+++ src/main/java/be/vinci/pae/presentation/UserRessource.java
@@ -20,11 +20,9 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
 import java.util.List;
 import org.glassfish.jersey.server.ContainerRequest;
 
