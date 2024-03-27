a16aabf7e49a46545aab01eb0c193e37d5c06e31
Hajar <hajar.haouat@student.vinci.be>
2024-03-27 14:20:19 +0100

    fix: return missing

--- src/main/java/be/vinci/pae/business/contact/Contact.java
+++ src/main/java/be/vinci/pae/business/contact/Contact.java
@@ -9,7 +9,7 @@ public interface Contact extends ContactDTO {
    * Update the state of the contact.
    *
    * @param state the new state of the contact
-   * return true if the state was updated, false otherwise
+   * @return true if the state was updated, false otherwise
    */
   boolean updateState(State state);
 }
