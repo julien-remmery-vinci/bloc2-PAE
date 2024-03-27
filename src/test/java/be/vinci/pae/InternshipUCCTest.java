232f6755dcc053b243bc617a776aa6366bb7c1da
Didia José Fechado <didia.josefechado@student.vinci.be>
2024-03-27 01:16:38 +0100

    feat: add test internship not exist

---
+++ src/test/java/be/vinci/pae/InternshipUCCTest.java
@@ -0,0 +1,54 @@
+package be.vinci.pae;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import be.vinci.pae.business.Factory;
+import be.vinci.pae.business.internship.InternshipDTO;
+import be.vinci.pae.business.internship.InternshipUCC;
+import be.vinci.pae.business.user.UserDTO;
+import be.vinci.pae.dal.internship.InternshipDAO;
+import be.vinci.pae.presentation.exceptions.NotFoundException;
+import java.util.ArrayList;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class InternshipUCCTest {
+
+  static ServiceLocator locator;
+  private static InternshipUCC internshipUCC;
+  private static InternshipDAO internshipDAO;
+  private static Factory factory;
+  InternshipDTO internship;
+
+  UserDTO user;
+
+  @BeforeAll
+  static void beforeAll() {
+    locator = ServiceLocatorUtilities.bind(new ApplicationBinderTest());
+    internshipUCC = locator.getService(InternshipUCC.class);
+    factory = locator.getService(Factory.class);
+    internshipDAO = locator.getService(InternshipDAO.class);
+  }
+
+  @BeforeEach
+  void setUp() {
+    internship = (InternshipDTO) factory.getInternship();
+    internship.setIdInternship(1);
+    user = (UserDTO) factory.getUser();
+    user.setIdUser(1);
+    user.setRole(UserDTO.Role.STUDENT);
+  }
+
+  @Test
+  @DisplayName("Test get internship by id method when the internship don't exists")
+  void testGetAllCompanies() {
+    Mockito.when(internshipDAO.getInternshipById(user.getIdUser())).thenReturn(new ArrayList<>());
+    assertThrows(NotFoundException.class, () -> internshipUCC.getInternshipById(user));
+  }
+
+}
