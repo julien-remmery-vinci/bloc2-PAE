9b90d6525c8c6db6f3f64a2517b06dd2d95ece6b
Semih Türkoglu <semih.turkoglu@student.vinci.be>
2024-04-03 23:09:30 +0200

    feat: adding getUserById method in API

--- webapp/src/Components/Pages/StudentInfoPage.js
+++ webapp/src/Components/Pages/StudentInfoPage.js
@@ -11,9 +11,36 @@ const StudentInfoPage = () => {
         renderPageTitle("Informations de l'étudiant");
         document.title = "Informations de l'étudiant";
         clearPage();
+        const id = window.location.pathname.split('/').pop();
+        fetchUserById(id);
         renderStudentInfoPage();
     } 
 };
+
+const fetchUserById = (id) => {
+  fetch(`http://localhost:3000/users${id}`)
+  .then(response => response.json())
+  .then(user => renderUser(user))
+  .catch(error => console.error(error));
+}
+
+const renderUser = (user) => {
+  const main = document.querySelector('main');
+  main.innerHTML = `
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Informations de l'étudiant</h1>
+        <p>Voici les informations de l'étudiant</p>
+        <p>Nom: ${user.name}</p>
+        <p>Prénom: ${user.firstname}</p>
+        <p>Email: ${user.email}</p>
+        <p>Role: ${user.role}</p>
+      </div>
+    </div>
+  </div>
+  `;
+}
 
 function renderStudentInfoPage() {
   const main = document.querySelector('main');
