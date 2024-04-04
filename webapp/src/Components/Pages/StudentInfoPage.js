311ea0f5563ae20ee4b40c9c2dd2c90e88453add
Semih Türkoglu <semih.turkoglu@student.vinci.be>
2024-04-04 11:14:23 +0200

    Refactor fetchUserById function and renderUser function in StudentInfoPage.js

--- webapp/src/Components/Pages/StudentInfoPage.js
+++ webapp/src/Components/Pages/StudentInfoPage.js
@@ -2,7 +2,35 @@ import { clearPage, renderPageTitle } from "../../utils/render";
 import Navigate from "../Router/Navigate";
 import {isAuthenticated} from "../../utils/auths"; // Import the 'isAuthenticated' function
 
-const renderUser = (user) => {
+const fetchUserById = async (id) => {
+  try {
+    fetch(`http://localhost:3000/users/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        renderUser(data);
+      })
+      .catch((error) => console.error(error));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const StudentInfoPage = () => {
+  if (!isAuthenticated()) {
+      Navigate('/login');
+  } else {
+      renderPageTitle("Informations de l'étudiant");
+      document.title = "Informations de l'étudiant";
+      clearPage();
+      const urlParams = new URLSearchParams(window.location.search);
+      const id = urlParams.get('id');
+      fetchUserById(id);
+      renderUser();
+  } 
+};
+
+
+function renderUser(user) {
   const main = document.querySelector('main');
   main.innerHTML = `
   <div class="container">
@@ -18,41 +46,6 @@ const renderUser = (user) => {
     </div>
   </div>
   `;
-}
-
-const fetchUserById = (id) => {
-  fetch(`http://localhost:3000/users${id}`)
-  .then(response => response.json())
-  .then(user => renderUser(user))
-  .catch(error => console.error(error));
-}
-
-const StudentInfoPage = () => {
-    if (!isAuthenticated()) {
-        Navigate('/login');
-    } else {
-        renderPageTitle("Informations de l'étudiant");
-        document.title = "Informations de l'étudiant";
-        clearPage();
-        const urlParams = new URLSearchParams(window.location.search);
-        const id = urlParams.get('id');
-        fetchUserById(id);
-        renderStudentInfoPage();
-    } 
 };
-
-function renderStudentInfoPage() {
-  const main = document.querySelector('main');
-  main.innerHTML = `
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1>Informations de l'étudiant</h1>
-        <p>Voici les informations de l'étudiant</p>
-      </div>
-    </div>
-  </div>
-  `;
-}
 
 export default StudentInfoPage;
