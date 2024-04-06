05cbc33018180bb595124100a8d0122c1860eb04
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-04-06 05:49:42 +0200

    feat: add getAll method to DAO of internship supervisor

--- src/main/java/be/vinci/pae/dal/internshipsupervisor/InternshipSupervisorDAO.java
+++ src/main/java/be/vinci/pae/dal/internshipsupervisor/InternshipSupervisorDAO.java
@@ -1,9 +1,18 @@
 package be.vinci.pae.dal.internshipsupervisor;
 
+import be.vinci.pae.business.internshipsupervisor.InternshipSupervisorDTO;
+import java.util.List;
+
 /**
  * Interface of InternshipSupervisorDAOImpl.
  */
 public interface InternshipSupervisorDAO {
 
+  /**
+   * Get all the internship supervisors.
+   *
+   * @return all a list of internship supervisors
+   */
+  List<InternshipSupervisorDTO> getAll();
 
 }
--- src/main/java/be/vinci/pae/dal/internshipsupervisor/InternshipSupervisorDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/internshipsupervisor/InternshipSupervisorDAOImpl.java
@@ -1,9 +1,40 @@
 package be.vinci.pae.dal.internshipsupervisor;
 
+import be.vinci.pae.business.internshipsupervisor.InternshipSupervisorDTO;
+import be.vinci.pae.dal.DALBackServices;
+import be.vinci.pae.dal.utils.DAOServices;
+import be.vinci.pae.presentation.exceptions.FatalException;
+import jakarta.inject.Inject;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Implementation of InternshipSupervisorDAOImpl.
  */
-public class InternshipSupervisorDAOImpl {
+public class InternshipSupervisorDAOImpl implements InternshipSupervisorDAO {
 
+  @Inject
+  private DALBackServices dalServices;
 
+  @Inject
+  private DAOServices daoServices;
+
+  @Override
+  public List<InternshipSupervisorDTO> getAll() {
+    List<InternshipSupervisorDTO> list = new ArrayList<>();
+    try (PreparedStatement ps = dalServices.getPS("SELECT * FROM pae.internshipSupervisors")) {
+      try (ResultSet rs = ps.executeQuery()) {
+        while (rs.next()) {
+          String prefix = "internshipSupervisor";
+          list.add((InternshipSupervisorDTO) daoServices.getDataFromRs(rs, prefix));
+        }
+        return list;
+      }
+    } catch (SQLException e) {
+      throw new FatalException(e);
+    }
+  }
 }
