14ad4626739a8ecba9a6d981df3990098150379e
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-04-06 05:48:26 +0200

    feat: Add getAll method to InternshipSupervisorRessource.java

--- src/main/java/be/vinci/pae/presentation/InternshipSupervisorRessource.java
+++ src/main/java/be/vinci/pae/presentation/InternshipSupervisorRessource.java
@@ -1,9 +1,30 @@
 package be.vinci.pae.presentation;
 
+import be.vinci.pae.business.internshipsupervisor.InternshipSupervisorDTO;
+import be.vinci.pae.business.internshipsupervisor.InternshipSupervisorUCC;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
 /**
  * InternshipSupervisor route.
  */
 
+@Singleton
+@Path("/internshipSupervisors")
 public class InternshipSupervisorRessource {
+
+  @Inject
+  private InternshipSupervisorUCC internshipSupervisorUCC;
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<InternshipSupervisorDTO> getAll() {
+    return internshipSupervisorUCC.getAll();
+  }
 
 }
