4b1ff8678f2f4f425f0269636bff51ef0efd748e
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-04-07 18:07:15 +0200

    refactor: Refactor InternshipSupervisorUCCImpl to get supervisors by company ID

--- src/main/java/be/vinci/pae/business/internshipsupervisor/InternshipSupervisorUCCImpl.java
+++ src/main/java/be/vinci/pae/business/internshipsupervisor/InternshipSupervisorUCCImpl.java
@@ -1,7 +1,9 @@
 package be.vinci.pae.business.internshipsupervisor;
 
 import be.vinci.pae.dal.DALServices;
+import be.vinci.pae.dal.company.CompanyDAO;
 import be.vinci.pae.dal.internshipsupervisor.InternshipSupervisorDAO;
+import be.vinci.pae.presentation.exceptions.NotFoundException;
 import jakarta.inject.Inject;
 import java.util.List;
 
@@ -16,11 +18,18 @@ public class InternshipSupervisorUCCImpl implements InternshipSupervisorUCC {
   @Inject
   private InternshipSupervisorDAO internshipSupervisorDAO;
 
+  @Inject
+  private CompanyDAO companyDAO;
+
   @Override
-  public List<InternshipSupervisorDTO> getAll() {
+  public List<InternshipSupervisorDTO> getSupervisorsByCompanyID(int idCompany) {
     try {
       dalServices.open();
-      return internshipSupervisorDAO.getAll();
+      CompanyDTO company = companyDAO.getCompanyById(idCompany);
+      if (company == null) {
+        throw new NotFoundException("Company not found");
+      }
+      return internshipSupervisorDAO.getSupervisorsByCompanyID(idCompany);
     } finally {
       dalServices.close();
     }
