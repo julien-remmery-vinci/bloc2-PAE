b2690f0d0dbf176716484d3411e2ad9a67a6f2a0
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-04-07 18:11:27 +0200

    refactor: Refactor InternshipSupervisorRessource to add endpoint for getting supervisors by company ID

--- src/main/java/be/vinci/pae/presentation/InternshipSupervisorRessource.java
+++ src/main/java/be/vinci/pae/presentation/InternshipSupervisorRessource.java
@@ -2,10 +2,12 @@ package be.vinci.pae.presentation;
 
 import be.vinci.pae.business.internshipsupervisor.InternshipSupervisorDTO;
 import be.vinci.pae.business.internshipsupervisor.InternshipSupervisorUCC;
+import be.vinci.pae.presentation.exceptions.BadRequestException;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import java.util.List;
@@ -22,9 +24,13 @@ public class InternshipSupervisorRessource {
   private InternshipSupervisorUCC internshipSupervisorUCC;
 
   @GET
+  @Path("/company/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public List<InternshipSupervisorDTO> getAll() {
-    return internshipSupervisorUCC.getAll();
+  public List<InternshipSupervisorDTO> getSupervisorsByCompanyID(@PathParam("id") int idCompany) {
+    if (idCompany < 0) {
+      throw new BadRequestException("Invalid id");
+    }
+    return internshipSupervisorUCC.getSupervisorsByCompanyID(idCompany);
   }
 
 }
