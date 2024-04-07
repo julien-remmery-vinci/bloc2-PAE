cf32c066aa4b95f74c70462ed3ce1d0691677d94
Semih Türkoglu <semih.turkoglu@student.vinci.be>
2024-04-07 18:08:51 +0200

    feat: Add search filters and styling to SearchPage.js

--- webapp/src/Components/Pages/SearchPage.js
+++ webapp/src/Components/Pages/SearchPage.js
@@ -26,19 +26,32 @@ const SearchPage = () => {
 function renderSearchPage() {
   const main = document.querySelector('main');
   main.innerHTML = `
+<div class="container">
+<div class="row">
+  <div class="col">
+    <h1>Recherche</h1>
+</div>
+<div class="row">
+<div class="col-md-4">
   <div class="search-container d-flex justify-content-between">
   <div class="filter-container">
     <h3>Filtres</h3>
     <label>
-      <input type="checkbox" name="filter" value="etudiant">
+      <input type="checkbox" id="studentFilter" name="filter" value="etudiant">
       Que les étudiants
     </label>
   </div>
-
+</div>
+</div>
+<div class="col-md-4 ms-auto">
   <div class="search-bar">
-    <input class="form-control border-end-0 border rounded-pill" type="search" value="Rechercher">
+    <input class="form-control rounded rounded-pill" type="search" value="Rechercher">
   </div>
 </div>
+<br>
+</div>
+</div>
+<br>
 <br>
     <div class="table-responsive d-flex justify-content-center align-items-center">
     <table class="table table-bordered table-hover caption-top">
