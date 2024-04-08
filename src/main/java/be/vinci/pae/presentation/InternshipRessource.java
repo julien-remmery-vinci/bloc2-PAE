b17976d06ce00c0c80a7c46222f8dea6499aa10f
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-04-08 23:10:20 +0200

    feat: Refactor InternshipRessource.java to add POST method for adding internships

--- src/main/java/be/vinci/pae/presentation/InternshipRessource.java
+++ src/main/java/be/vinci/pae/presentation/InternshipRessource.java
@@ -3,11 +3,14 @@ package be.vinci.pae.presentation;
 import be.vinci.pae.business.internship.InternshipDTO;
 import be.vinci.pae.business.internship.InternshipUCC;
 import be.vinci.pae.business.user.UserDTO;
+import be.vinci.pae.presentation.exceptions.BadRequestException;
 import be.vinci.pae.presentation.exceptions.NotFoundException;
 import be.vinci.pae.presentation.filters.Authorize;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
@@ -39,6 +42,27 @@ public class InternshipRessource {
       throw new NotFoundException("User not found");
     }
     return internshipUCC.getInternshipById(user);
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public InternshipDTO addInternship(@Context ContainerRequest request, InternshipDTO internship) {
+    if (internship.getIdCompany() < 0) {
+      throw new BadRequestException("Invalid Company id");
+    }
+    if (internship.getIdInternshipSupervisor() < 0) {
+      throw new BadRequestException("Invalid Internship Supervisor id");
+    }
+    if (internship.getIdContact() < 0) {
+      throw new BadRequestException("Invalid Contact id");
+    }
+    if (internship.getIdStudent() < 0) {
+      throw new BadRequestException("Invalid Student id");
+    }
+    internship.setIdStudent(((UserDTO) request.getProperty("user")).getIdUser());
+    return internshipUCC.addInternship(internship);
   }
 }
 
