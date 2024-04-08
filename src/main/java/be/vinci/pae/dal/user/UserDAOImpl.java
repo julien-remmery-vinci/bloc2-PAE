48c4bcb8619813732f63801fd0822545f7d09503
Didia José Fechado <didia.josefechado@student.vinci.be>
2024-04-08 22:07:05 +0200

    bugfix: execute update

--- src/main/java/be/vinci/pae/dal/user/UserDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/user/UserDAOImpl.java
@@ -114,20 +114,21 @@ public class UserDAOImpl implements UserDAO {
             + " user_email = ?, user_password = ?, user_phoneNumber = ?,"
             + " user_registerDate = ?, user_role = ?, user_academicYear = ?, "
             + "user_version = ? WHERE user_idUser = ?"
-            + " AND user_version = ?")) {
+            + " AND user_version = ? RETURNING user_idUser")) {
       setPs(updateUser, user);
       updateUser.setInt(9, user.getVersion() + 1);
       updateUser.setInt(10, user.getIdUser());
       updateUser.setInt(11, user.getVersion());
-      updateUser.executeUpdate();
-      return user;
+      try (ResultSet rs = updateUser.executeQuery()) {
+        if (getOneById(user.getIdUser()) == null) {
+          throw new NotFoundException("User not found");
+        }
+        if (!rs.next() && getOneById(user.getIdUser()).getVersion() != user.getVersion()) {
+          throw new ConflictException("Version mismatch");
+        }
+        return user;
+      }
     } catch (SQLException e) {
-      if (getOneById(user.getIdUser()) == null) {
-        throw new NotFoundException("User not found");
-      }
-      if (getOneById(user.getIdUser()).getVersion() != user.getVersion()) {
-        throw new ConflictException("Version mismatch");
-      }
       throw new FatalException(e);
     }
   }
