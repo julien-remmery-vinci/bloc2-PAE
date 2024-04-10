901848ec541602c7fa1d943a61e9d03dcfa411b4
Hadjera Emroska <hadjera.emroska@student.vinci.be>
2024-04-11 01:47:45 +0200

    fix: typo in InternshipDAOImpl.java

--- src/main/java/be/vinci/pae/dal/internship/InternshipDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/internship/InternshipDAOImpl.java
@@ -59,7 +59,7 @@ public class InternshipDAOImpl implements InternshipDAO {
     try (PreparedStatement ps = dalServices.getPS(
         "INSERT INTO pae.internships (internship_idCompany, internship_idStudent, "
             + "internship_idContact, internship_idInternshipSupervisor, "
-            + "internship_internshipprojet, "
+            + "internship_internshipproject, "
             + "internship_signatureDate, internship_version) "
             + "VALUES (?, ?, ?, ?, ?, ?, 1) RETURNING internship_idInternship;")) {
       ps.setInt(1, internship.getIdCompany());
