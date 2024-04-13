5ce3f02c2d8fd8bd6d24c00b264e7bd0cce3b954
Hajar <hajar.haouat@student.vinci.be>
2024-04-13 18:56:53 +0200

    fix: add path to ressource

--- src/main/java/be/vinci/pae/presentation/InternshipRessource.java
+++ src/main/java/be/vinci/pae/presentation/InternshipRessource.java
@@ -95,6 +95,7 @@ public class InternshipRessource {
   @PUT
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
+  @Path("/{id}")
   @Authorize(roles = {Role.STUDENT})
   public InternshipDTO updateInternshipSubject(JsonNode json, @Context ContainerRequest request) {
     InternshipDTO internship = internshipUCC.getInternshipById((UserDTO) request.getProperty("user"));
